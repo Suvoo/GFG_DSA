81ac7f165f7f51efa2a70864a1215d3a0e640541
Suvodeep Sinha <suvodeepsinha1981@gmail.com>
2020-08-15 15:18:31 +0530

    Delete AdditionUnderMod.cpp

--- GFG_COURSE/Arrays/AdditionUnderMod.cpp
+++
@@ -1,9 +0,0 @@
-/* https://practice.geeksforgeeks.org/problems/addition-under-modulo/1/?track=ppc-mathematics&batchId=221 */
-
-int sumUnderModulo(long long a,long long b)
-{
-    int M=1000000007;
-    //your code here
-    
-    return (a%M + b%M)%M;
-}
