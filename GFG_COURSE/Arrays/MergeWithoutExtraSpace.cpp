db39aa0fa03a0beefb94bd4ae50e1d9293b5e99f
Suvodeep Sinha <suvodeepsinha1981@gmail.com>
2020-08-18 18:17:26 +0530

    TwoApproach (Insertionsort and Gap) SS-Tier

---
+++ GFG_COURSE/Arrays/MergeWithoutExtraSpace.cpp
@@ -0,0 +1,88 @@
+/* https://practice.geeksforgeeks.org/problems/merge-two-sorted-arrays-1587115620/1/?track=ppc-arrays&batchId=221 */
+
+/* REFERENCES : https://www.geeksforgeeks.org/efficiently-merging-two-sorted-arrays-with-o1-extra-space/
+                https://www.techiedelight.com/inplace-merge-two-sorted-arrays/
+                https://youtu.be/hVl2b3bLzBw
+*/
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+
+
+// Function to merge two input arrays
+// arr1[], arr2[]: input arrays
+// n, m: size of arr1[] and arr2[] respectively
+// Merge ar1[] and ar2[] with O(1) extra space 
+int nextGap(int gap) 
+{ 
+    if (gap <= 1) 
+        return 0; 
+    return (gap / 2) + (gap % 2); 
+} 
+  
+void merge(int *arr1, int *arr2, int n, int m) 
+{ 
+    int i, j, gap = n + m; 
+    for (gap = nextGap(gap); gap > 0; gap = nextGap(gap)) 
+    { 
+        // comparing elements in the first array. 
+        for (i = 0; i + gap < n; i++) 
+            if (arr1[i] > arr1[i + gap]) 
+                swap(arr1[i], arr1[i + gap]); 
+  
+        //comparing elements in both arrays. 
+        for (j = gap > n ? gap-n : 0 ; i < n&&j < m; i++, j++) 
+            if (arr1[i] > arr2[j]) 
+                swap(arr1[i], arr2[j]); 
+  
+        if (j < m) 
+        { 
+            //comparing elements in the second array. 
+            for (j = 0; j + gap < m; j++) 
+                if (arr2[j] > arr2[j + gap]) 
+                    swap(arr2[j], arr2[j + gap]); 
+        } 
+    } 
+} 
+  
+
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+	
+	int T;
+	cin >> T;
+	
+	while(T--){
+	    int n, m;
+	    cin >> n >> m;
+	    
+	    int arr1[n], arr2[m];
+	    
+	    for(int i = 0;i<n;i++){
+	        cin >> arr1[i];
+	    }
+	    
+	    for(int i = 0;i<m;i++){
+	        cin >> arr2[i];
+	    }
+	    
+	    merge(arr1, arr2, n, m); 
+
+        for (int i = 0; i < n; i++) 
+            printf("%d ", arr1[i]); 
+        
+       
+	    for (int i = 0; i < m; i++) 
+		    printf("%d ", arr2[i]); 
+	    
+	    cout<<endl;
+	}
+
+	return 0; 
+} 
+  // } Driver Code Ends
