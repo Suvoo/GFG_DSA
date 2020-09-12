27968f7581409724d7d31bf7b29d2a67465e303f
Suvodeep Sinha <suvodeepsinha1981@gmail.com>
2020-09-12 10:22:10 +0530

    reverse(imp)

---
+++ GFG_COURSE/Linked Lists/ReverseList.cpp
@@ -0,0 +1,112 @@
+/* https://practice.geeksforgeeks.org/problems/reverse-a-linked-list/1/?track=ppc-linkedlist&batchId=221 */
+/* Also check recursion for this problem : 
+Node reverse(Node head)
+   {
+        if(head.next == null) return head;
+        Node newHead = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+   }
+   */
+
+//Initial Template for C++
+
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+/* Function to get the middle of the linked list*/
+struct Node *reverseList(struct Node *head);
+
+void printList(struct Node *head)
+{
+    struct Node *temp = head;
+    while (temp != NULL)
+    {
+       printf("%d ", temp->data);
+       temp  = temp->next;
+    }
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    int T,n,l,firstdata;
+    cin>>T;
+
+    while(T--)
+    {
+        struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        
+        cin>>firstdata;
+        head = new Node(firstdata);
+        tail = head;
+        
+        for (int i=1; i<n; i++)
+        {
+            cin>>l;
+            tail->next = new Node(l);
+            tail = tail->next;
+        }
+        
+        head = reverseList(head);
+        
+        printList(head);
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+/* Linked List Node structure:
+
+struct Node
+{
+    int data;
+    struct Node *next;
+}
+
+*/
+
+// Should reverse list and return new head.
+struct Node* reverseList(struct Node *head)
+{
+     // Initialize current, previous and 
+        // next pointers 
+        Node* current = head; 
+        Node *prev = NULL, *next = NULL; 
+  
+        while (current != NULL) { 
+            // Store next 
+            next = current->next; 
+  
+            // Reverse current node's pointer 
+            current->next = prev; 
+  
+            // Move pointers one position ahead. 
+            prev = current; 
+            current = next; 
+        } 
+        head = prev; 
+        
+        return head;
+}
